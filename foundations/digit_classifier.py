82b23887467ca412354e0f7162a49395e595f12c
Bacem Karray <99637476+bacemkarray@users.noreply.github.com>
2026-05-15 10:33:16 -0400

    Update Digit classifier (Training 4/4)

--- foundations/digit_classifier.py
+++ foundations/digit_classifier.py
@@ -6,19 +6,15 @@ class Solution(nn.Module):
     def __init__(self):
         super().__init__()
         torch.manual_seed(0)
-        pass
-        # Define the architecture here
-        self.layer = nn.Sequential(
-            nn.Linear(in_features=784, out_features=512),
-            nn.ReLU(),
-            nn.Dropout(p=0.2),
-            nn.Linear(in_features=512, out_features=10),
-            nn.Sigmoid()
-        )
-    
+        # Architecture: Linear(784, 512) -> ReLU -> Dropout(0.2) -> Linear(512, 10) -> Sigmoid
+        self.layer_1 = nn.Linear(in_features=784, out_features=512)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.2)
+        self.layer_2 = nn.Linear(in_features=512,out_features=10)
+        self.sigmoid = nn.Sigmoid()
+
     def forward(self, images: TensorType[float]) -> TensorType[float]:
         torch.manual_seed(0)
-        pass
+        # images shape: (batch_size, 784)
         # Return the model's prediction to 4 decimal places
-        return self.layer(images)
-
+        return self.sigmoid(self.layer_2(self.dropout(self.relu(self.layer_1(images)))))
